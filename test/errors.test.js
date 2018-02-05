3970ff2cccd8cc5c3810348c01babc244379bcaa
Dave Houlbrooke <dave@shax.com>
2018-02-05 00:22:56 +0000

    Remove unused files

--- test/errors.test.js
+++
@@ -1,9 +0,0 @@
-const { BlorkError } = require('../lib/errors');
-
-// Tests.
-describe('debug()', () => {
-	test('Return the correct debug string for different types', () => {
-		expect(new BlorkError()).toHaveProperty('message');
-		expect(new BlorkError('abc')).toHaveProperty('message', 'abc');
-	});
-});
