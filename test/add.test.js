5291386657161a743abbb8051839f98eba12fbcc
Dave Houlbrooke <dave@shax.com>
2018-03-27 04:44:45 +0100

    add() can have a fourth arg that allows you to set a custom error
    Also FormattedErrors receive all the format() args so they can format them themselves.

--- test/add.test.js
+++ test/add.test.js
@@ -1,29 +1,58 @@
+const FormattedError = require("../lib/FormattedError");
 const BlorkError = require("../lib/BlorkError");
 const { check, add } = require("../lib/exports");
 
 // Tests.
 describe("add()", () => {
-	test("Add and run a custom checker (with description)", () => {
-		// Define a checker called 'isstring'.
-		expect(add("isstring", v => typeof v === "string", "valid test checker")).toBeUndefined();
-
-		// Check a passing value.
-		expect(check("abc", "isstring")).toBe(undefined);
-
-		// Check a failing value.
-		expect(() => check(123, "isstring")).toThrow(TypeError);
-		expect(() => check(123, "isstring")).toThrow(/valid test checker/); // Must contain description.
-	});
 	test("Add and run a custom checker (no description)", () => {
-		// Define a checker called 'isnumber'.
-		expect(add("isnumber", v => typeof v === "number")).toBeUndefined();
+		// Define a checker called '11218c'.
+		expect(add("11218c", v => typeof v === "string")).toBeUndefined();
 
 		// Check a passing value.
-		expect(check(123, "isnumber")).toBe(undefined);
+		expect(check("abc", "11218c")).toBe(undefined);
 
 		// Check a failing value.
-		expect(() => check("abc", "isnumber")).toThrow(TypeError);
-		expect(() => check("abc", "isnumber")).toThrow(/isnumber/); // Must contain name.
+		expect(() => check(123, "11218c")).toThrow(TypeError);
+		expect(() => check(123, "11218c")).toThrow(/11218c/); // Must contain name.
+	});
+	test("Add and run a custom checker (with description)", () => {
+		// Define a checker called '618e0e'.
+		expect(add("618e0e", v => typeof v === "string", "a43829")).toBeUndefined();
+
+		// Check a passing value.
+		expect(check("abc", "618e0e")).toBe(undefined);
+
+		// Check a failing value.
+		expect(() => check(123, "618e0e")).toThrow(TypeError);
+		expect(() => check(123, "618e0e")).toThrow(/a43829/); // Must contain description.
+	});
+	test("Add and run a custom checker (with custom Error)", () => {
+		// Create a custom error.
+		class IsBooleanError extends Error {}
+
+		// Define a checker called 'eee400'.
+		expect(add("eee400", v => typeof v === "string", "4c685f", IsBooleanError)).toBeUndefined();
+
+		// Check a passing value.
+		expect(check("abc", "eee400")).toBe(undefined);
+
+		// Check a failing value.
+		expect(() => check(123, "eee400")).toThrow(IsBooleanError);
+		expect(() => check(123, "eee400")).toThrow(/4c685f/); // Must contain description.
+	});
+	test("Add and run a custom checker (with custom FormattedError)", () => {
+		// Create a custom error.
+		class IsNullError extends FormattedError {}
+
+		// Define a checker called '013e93'.
+		expect(add("013e93", v => typeof v === "string", "824b7c", IsNullError)).toBeUndefined();
+
+		// Check a passing value.
+		expect(check("abc", "013e93")).toBe(undefined);
+
+		// Check a failing value.
+		expect(() => check(123, "013e93")).toThrow(IsNullError);
+		expect(() => check(123, "013e93")).toThrow(/824b7c/); // Must contain description.
 	});
 	test("Throw BlorkError if not non-empty lowercase string", () => {
 		const func = () => {};
