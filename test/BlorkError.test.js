7293efde68370cf619f42c50a1fb56994dfb51b2
Dave Houlbrooke <dave@shax.com>
2018-03-27 04:41:40 +0100

    BlorkError extends FormattedError
    Update tests to match

--- test/BlorkError.test.js
+++ test/BlorkError.test.js
@@ -2,7 +2,13 @@ const BlorkError = require("../lib/BlorkError");
 
 // Tests.
 describe("BlorkError()", () => {
-	test("Return the correct debug string for different types", () => {
-		expect(new BlorkError("abc")).toHaveProperty("message", "abc");
+	test("Return correct error with message only", () => {
+		expect(new BlorkError("Message")).toHaveProperty("message", "Message");
+	});
+	test("Return correct error with message and value", () => {
+		expect(new BlorkError("Message", 123)).toHaveProperty("message", "Message (received 123)");
+	});
+	test("Return correct error with message, value, and prefix", () => {
+		expect(new BlorkError("Message", 123, "Prefix")).toHaveProperty("message", "Prefix: Message (received 123)");
 	});
 });
