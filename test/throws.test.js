3a7d2fba3e240fac8bdf189500abcc85e07e3006
Dave Houlbrooke <dave@shax.com>
2018-03-27 04:43:25 +0100

    Tests for throws() using normal error and FormattedError

--- test/throws.test.js
+++ test/throws.test.js
@@ -1,3 +1,4 @@
+const FormattedError = require("../lib/FormattedError");
 const BlorkError = require("../lib/BlorkError");
 const { check, throws } = require("../lib/exports");
 
@@ -11,7 +12,20 @@ describe("throws()", () => {
 		expect(throws(MyError)).toBeUndefined();
 
 		// Fail a check and make sure it throws the custom error (not TypeError).
-		expect(() => check(false, "true")).toThrow(MyError);
+		expect(() => check(false, "true", "myValue")).toThrow(MyError);
+		expect(() => check(false, "true", "myValue")).toThrow(/myValue: Must be true \(received false\)/);
+	});
+	test("Set a custom error object (that extends FormattedError) and check it throws", () => {
+		// Define a custom error.
+		class MyFormattedError extends FormattedError {}
+
+		// Set it as the custom error.
+		expect(throws(MyFormattedError)).toBeUndefined();
+
+		// Fail a check and make sure it throws the custom error (not TypeError).
+		expect(() => check(false, "true", "myValue")).toThrow(MyFormattedError);
+		expect(() => check(false, "true", "myValue")).toThrow(FormattedError);
+		expect(() => check(false, "true", "myValue")).toThrow(/myValue: Must be true \(received false\)/);
 	});
 	test("Throw BlorkError if passing a non-function", () => {
 		expect(() => throws(false)).toThrow(BlorkError);
