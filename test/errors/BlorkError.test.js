988c34a16e0393b65fbfa7b1991a735c55249b23
Dave Houlbrooke <dave@shax.com>
2018-04-02 22:59:49 +0100

    More robust undefined value checking for format()

--- test/errors/BlorkError.test.js
+++ test/errors/BlorkError.test.js
@@ -2,13 +2,27 @@ const BlorkError = require("../../lib/errors/BlorkError");
 
 // Tests.
 describe("BlorkError()", () => {
+	test("Return correct error with default message", () => {
+		expect(new BlorkError()).toHaveProperty("message");
+		expect(new BlorkError()).toHaveProperty("reason");
+		expect(new BlorkError()).not.toHaveProperty("value");
+	});
 	test("Return correct error with message only", () => {
 		expect(new BlorkError("Message")).toHaveProperty("message", "Message");
+		expect(new BlorkError("Message")).toHaveProperty("prefix", "");
+		expect(new BlorkError("Message")).toHaveProperty("reason", "Message");
+		expect(new BlorkError("Message")).not.toHaveProperty("value");
 	});
 	test("Return correct error with message and value", () => {
 		expect(new BlorkError("Message", 123)).toHaveProperty("message", "Message (received 123)");
+		expect(new BlorkError("Message", 123)).toHaveProperty("prefix", "");
+		expect(new BlorkError("Message", 123)).toHaveProperty("reason", "Message");
+		expect(new BlorkError("Message", 123)).toHaveProperty("value", 123);
 	});
 	test("Return correct error with message, value, and prefix", () => {
 		expect(new BlorkError("Message", 123, "Prefix")).toHaveProperty("message", "Prefix: Message (received 123)");
+		expect(new BlorkError("Message", 123, "Prefix")).toHaveProperty("prefix", "Prefix");
+		expect(new BlorkError("Message", 123, "Prefix")).toHaveProperty("reason", "Message");
+		expect(new BlorkError("Message", 123, "Prefix")).toHaveProperty("value", 123);
 	});
 });
---
+++ test/errors/ValueError.test.js
@@ -0,0 +1,33 @@
+const ValueError = require("../../lib/errors/ValueError");
+
+// Tests.
+describe("ValueError()", () => {
+	test("Return correct error with default message", () => {
+		expect(new ValueError()).toHaveProperty("name", "ValueError");
+		expect(new ValueError()).toHaveProperty("message", "Invalid value");
+		expect(new ValueError()).toHaveProperty("prefix", "");
+		expect(new ValueError()).toHaveProperty("reason", "Invalid value");
+		expect(new ValueError()).not.toHaveProperty("value");
+	});
+	test("Return correct error with message only", () => {
+		expect(new ValueError("Message")).toHaveProperty("name", "ValueError");
+		expect(new ValueError("Message")).toHaveProperty("message", "Message");
+		expect(new ValueError("Message")).toHaveProperty("prefix", "");
+		expect(new ValueError("Message")).toHaveProperty("reason", "Message");
+		expect(new ValueError("Message")).not.toHaveProperty("value");
+	});
+	test("Return correct error with message and value", () => {
+		expect(new ValueError("Message", 123)).toHaveProperty("name", "ValueError");
+		expect(new ValueError("Message", 123)).toHaveProperty("message", "Message (received 123)");
+		expect(new ValueError("Message", 123)).toHaveProperty("prefix", "");
+		expect(new ValueError("Message", 123)).toHaveProperty("reason", "Message");
+		expect(new ValueError("Message", 123)).toHaveProperty("value", 123);
+	});
+	test("Return correct error with message, value, and prefix", () => {
+		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("name", "ValueError");
+		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("message", "Prefix: Message (received 123)");
+		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("prefix", "Prefix");
+		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("reason", "Message");
+		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("value", 123);
+	});
+});
