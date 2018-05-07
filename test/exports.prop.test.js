4b960e8de0380c1fb9da5b58922ae9f32f66efd9
Dave Houlbrooke <dave@shax.com>
2018-05-07 02:43:37 +0100

    100% coverage tests for prop()

--- test/exports.prop.test.js
+++ test/exports.prop.test.js
@@ -48,8 +48,14 @@ describe("exports.prop()", () => {
 		expect(() => (obj.optionalStr = undefined)).not.toThrow(Error);
 		// Throws.
 		expect(() => (obj.bool = 123)).toThrow(ValueError);
+		expect(() => (obj.bool = 123)).toThrow(/bool:/);
+		expect(() => (obj.bool = 123)).toThrow(/true or false/);
 		expect(() => (obj.numOrStr = true)).toThrow(ValueError);
+		expect(() => (obj.numOrStr = true)).toThrow(/numOrStr:/);
+		expect(() => (obj.numOrStr = true)).toThrow(/number or string/);
 		expect(() => (obj.optionalStr = 123)).toThrow(ValueError);
+		expect(() => (obj.optionalStr = 123)).toThrow(/optionalStr/);
+		expect(() => (obj.optionalStr = 123)).toThrow(/string or empty/);
 	});
 	test("Property can be locked to explicit literal type", () => {
 		const obj = {};
@@ -67,11 +73,37 @@ describe("exports.prop()", () => {
 	});
 	test("Property can be locked to explicit object type", () => {
 		const obj = {};
-		prop(obj, "objStr", { str: "ABC" }, { str: "string" });
+		prop(obj, "objWithStr", { str: "ABC" }, { str: "string" });
 		// Doesn't throw.
-		expect(() => (obj.objStr = { str: "DEF" })).not.toThrow(Error);
+		expect(() => (obj.objWithStr = { str: "DEF" })).not.toThrow(Error);
 		// Throws.
-		expect(() => (obj.objStr = { str: 123 })).toThrow(ValueError);
+		expect(() => (obj.objWithStr = 123)).toThrow(ValueError);
+		expect(() => (obj.objWithStr = 123)).toThrow(/objWithStr:/);
+		expect(() => (obj.objWithStr = 123)).toThrow(/plain object/);
+		expect(() => (obj.objWithStr = { str: 123 })).toThrow(ValueError);
+		expect(() => (obj.objWithStr = { str: 123 })).toThrow(/objWithStr\.str:/);
+		expect(() => (obj.objWithStr = { str: 123 })).toThrow(/string/);
+	});
+	test("Property can be locked to implied plain object type", () => {
+		const obj = {};
+		prop(obj, "obj", { str: "ABC" }); // Plain object.
+		// Doesn't throw.
+		expect(() => (obj.obj = {})).not.toThrow(Error); // Any instanceof Object is fine.
+		// Throws.
+		expect(() => (obj.obj = false)).toThrow(ValueError);
+		expect(() => (obj.obj = false)).toThrow(/obj:/);
+		expect(() => (obj.obj = false)).toThrow(/instance of Object/);
+	});
+	test("Property can be locked to implied class object type", () => {
+		const obj = {};
+		class Dog {}
+		prop(obj, "dog", new Dog()); // Dog object.
+		// Doesn't throw.
+		expect(() => (obj.dog = new Dog())).not.toThrow(Error);
+		// Throws.
+		expect(() => (obj.dog = {})).toThrow(ValueError);
+		expect(() => (obj.dog = {})).toThrow(/dog:/);
+		expect(() => (obj.dog = {})).toThrow(/instance of Dog/);
 	});
 	test("Locked property cannot be deleted", () => {
 		const obj = {};
@@ -83,19 +115,19 @@ describe("exports.prop()", () => {
 	});
 	test("Throw BlorkError if passing incorrect object argument", () => {
 		expect(() => prop(null)).toThrow(BlorkError); // Must be BlorkError.
-		expect(() => prop(null)).toThrow(/lock\(\):/); // Must contain "prop()"
+		expect(() => prop(null)).toThrow(/prop\(\):/); // Must contain "prop()"
 		expect(() => prop(null)).toThrow(/object:/); // Must contain "object:"
 		expect(() => prop(null)).toThrow(/must be object/i); // Must contain "must be object"
 	});
 	test("Throw BlorkError if passing incorrect name argument", () => {
 		expect(() => prop({}, null)).toThrow(BlorkError); // Must be BlorkError.
-		expect(() => prop({}, null)).toThrow(/lock\(\):/); // Must contain "prop()"
+		expect(() => prop({}, null)).toThrow(/prop\(\):/); // Must contain "prop()"
 		expect(() => prop({}, null)).toThrow(/name:/); // Must contain "arguments[1]"
 		expect(() => prop({}, null)).toThrow(/string/); // Must contain "must be string"
 	});
 	test("Throw BlorkError if type is specified and value doesn't match", () => {
 		expect(() => prop({}, "prop", "abc", "number")).toThrow(BlorkError); // Must be BlorkError.
-		expect(() => prop({}, "prop", "abc", "number")).toThrow(/lock\(\)/); // Must contain "prop()"
+		expect(() => prop({}, "prop", "abc", "number")).toThrow(/prop\(\)/); // Must contain "prop()"
 		expect(() => prop({}, "prop", "abc", "number")).toThrow(/value:/); // Must contain "arguments[2]"
 		expect(() => prop({}, "prop", "abc", "number")).toThrow(/number/); // Must contain "number"
 		expect(() => prop({}, "prop", 123, "string")).toThrow(/string/); // Must contain "must be string"
