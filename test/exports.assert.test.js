a4a3d9fe02807c8b2514749abccf1964aa50fb51
Dave Houlbrooke <dave@shax.com>
2018-07-04 16:33:48 +0100

    assert(): Improvements to description format

--- test/exports.assert.test.js
+++ test/exports.assert.test.js
@@ -4,11 +4,11 @@ const { assert, BlorkError, ValueError } = require("../lib/exports");
 describe("exports.assert()", () => {
 	describe("assertion, description", () => {
 		test("Do nothing if assertion is true", () => {
-			expect(() => assert(true, "assertively true")).not.toThrow();
+			expect(() => assert(true, "be assertively true")).not.toThrow();
 		});
 		test("Throw BlorkError if assertion is false", () => {
-			expect(() => assert(false, "assertively true")).toThrow(ValueError);
-			expect(() => assert(false, "assertively true")).toThrow(/Must be assertively true/);
+			expect(() => assert(false, "be assertively true")).toThrow(ValueError);
+			expect(() => assert(false, "be assertively true")).toThrow(/Must be assertively true/);
 		});
 		test("Throw BlorkError if description is not string", () => {
 			expect(() => assert(true, 123)).toThrow(BlorkError);
@@ -17,21 +17,21 @@ describe("exports.assert()", () => {
 	});
 	describe("prefix", () => {
 		test("Error prefix defaults to no prefix", () => {
-			expect(() => assert(false, "assertively true")).toThrow(/^Must/);
+			expect(() => assert(false, "be assertively true")).toThrow(/^Must/);
 		});
 		test("Error prefix can be altered by setting prefix argument", () => {
-			expect(() => assert(false, "assertively true", "myprefix")).toThrow(/^myprefix/);
+			expect(() => assert(false, "be assertively true", "myprefix")).toThrow(/^myprefix/);
 		});
 	});
 	describe("error", () => {
 		test("Error class defaults to ValueError", () => {
-			expect(() => assert(false, "assertively true", "a")).toThrow(ValueError);
-			expect(() => assert(false, "assertively true", "a")).toThrow(ValueError);
+			expect(() => assert(false, "be assertively true", "a")).toThrow(ValueError);
+			expect(() => assert(false, "be assertively true", "a")).toThrow(ValueError);
 		});
 		test("Error class can be altered by setting error argument", () => {
 			class MyError extends Error {}
-			expect(() => assert(false, "assertively true", "a", MyError)).toThrow(MyError);
-			expect(() => assert(false, "assertively true", "a", MyError)).toThrow(MyError);
+			expect(() => assert(false, "be assertively true", "a", MyError)).toThrow(MyError);
+			expect(() => assert(false, "be assertively true", "a", MyError)).toThrow(MyError);
 		});
 	});
 });
