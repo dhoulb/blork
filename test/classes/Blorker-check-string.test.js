1ee19936e2209dbbe91d4f837f48b0c2d59ebf5c
Dave Houlbrooke <dave@shax.com>
2018-08-18 14:33:46 +0100

    modifiers: Create a `return` string type
    - Used like `check(123, "return str");
    Changes thrown error messages from `Must be string` to `Must return string`

--- test/classes/Blorker-check-string.test.js
+++ test/classes/Blorker-check-string.test.js
@@ -439,4 +439,18 @@ describe("exports.check() string types", () => {
 			expect(check("abc", "(num | (string) | num)")).toBe(undefined);
 		});
 	});
+	describe("Return types", () => {
+		test("Return types pass correctly", () => {
+			expect(check("a", "return string")).toBe(undefined);
+			expect(check(123, "return number")).toBe(undefined);
+		});
+		test("Return types fail correctly", () => {
+			expect(() => check(123, "return string")).toThrow(TypeError);
+			expect(() => check("a", "return number")).toThrow(TypeError);
+		});
+		test("Correct error message", () => {
+			expect(() => check(123, "return string")).toThrow(/Must return string/);
+			expect(() => check("a", "return number")).toThrow(/Must return finite number/);
+		});
+	});
 });
