4e32df77ad13d9b361c595049c9d2c9f35f9078b
Dave Houlbrooke <dave@shax.com>
2018-08-18 14:19:11 +0100

    ValueError: Don't auto-prefix function name if prefix includes ':' colon
    (The reason a prefix would include a colon is likely because function name has been baked into the prefix itself.)

--- test/errors/ValueError.test.js
+++ test/errors/ValueError.test.js
@@ -2,63 +2,109 @@ const { ValueError, EMPTY } = require("../../lib/exports");
 
 // Tests.
 describe("ValueError()", () => {
-	test("Return correct error with default message", () => {
-		expect(new ValueError()).toHaveProperty("name", "ValueError");
-		expect(new ValueError()).toHaveProperty("message", "Object.test(): Invalid value");
-		expect(new ValueError()).toHaveProperty("prefix", "Object.test()");
-		expect(new ValueError()).toHaveProperty("reason", "Invalid value");
-		expect(new ValueError()).not.toHaveProperty("value");
+	describe("Default reason", () => {
+		test("Return correct error with default reason (named function)", () => {
+			function abc() {
+				const e = new ValueError();
+				expect(e).toHaveProperty("name", "ValueError");
+				expect(e).toHaveProperty("message", "abc(): Invalid value");
+				expect(e).toHaveProperty("prefix", "abc()");
+				expect(e).toHaveProperty("reason", "Invalid value");
+				expect(e).not.toHaveProperty("value");
+			}
+			abc();
+		});
+		test("Return correct error with default reason (named function)", () => {
+			(() => {
+				const e = new ValueError();
+				expect(e).toHaveProperty("name", "ValueError");
+				expect(e).toHaveProperty("message", "Invalid value");
+				expect(e).toHaveProperty("prefix", "");
+				expect(e).toHaveProperty("reason", "Invalid value");
+				expect(e).not.toHaveProperty("value");
+			})();
+		});
 	});
-	test("Return correct error with message only", () => {
-		expect(new ValueError("Message")).toHaveProperty("name", "ValueError");
-		expect(new ValueError("Message")).toHaveProperty("message", "Object.test(): Message");
-		expect(new ValueError("Message")).toHaveProperty("prefix", "Object.test()");
-		expect(new ValueError("Message")).toHaveProperty("reason", "Message");
-		expect(new ValueError("Message")).not.toHaveProperty("value");
-		function abc() {
-			return new ValueError("Message");
-		}
-		expect(abc()).toHaveProperty("name", "ValueError");
-		expect(abc()).toHaveProperty("message", "abc(): Message");
-		expect(abc()).toHaveProperty("prefix", "abc()");
-		expect(abc()).toHaveProperty("reason", "Message");
-		expect(abc()).not.toHaveProperty("value");
+	describe("Reason", () => {
+		test("Return correct error with reason only (named function)", () => {
+			function abc() {
+				const e = new ValueError("Reason");
+				expect(e).toHaveProperty("name", "ValueError");
+				expect(e).toHaveProperty("message", "abc(): Reason");
+				expect(e).toHaveProperty("prefix", "abc()");
+				expect(e).toHaveProperty("reason", "Reason");
+				expect(e).not.toHaveProperty("value");
+			}
+			abc();
+		});
+		test("Return correct error with reason only (named function)", () => {
+			(() => {
+				const e = new ValueError("Reason");
+				expect(e).toHaveProperty("name", "ValueError");
+				expect(e).toHaveProperty("message", "Reason");
+				expect(e).toHaveProperty("prefix", "");
+				expect(e).toHaveProperty("reason", "Reason");
+				expect(e).not.toHaveProperty("value");
+			})();
+		});
 	});
-	test("Return correct error with message and value", () => {
-		expect(new ValueError("Message", 123)).toHaveProperty("name", "ValueError");
-		expect(new ValueError("Message", 123)).toHaveProperty("message", "Object.test(): Message (received 123)");
-		expect(new ValueError("Message", 123)).toHaveProperty("prefix", "Object.test()");
-		expect(new ValueError("Message", 123)).toHaveProperty("reason", "Message");
-		expect(new ValueError("Message", 123)).toHaveProperty("value", 123);
-		function abc() {
-			return new ValueError("Message", 123);
-		}
-		expect(abc()).toHaveProperty("name", "ValueError");
-		expect(abc()).toHaveProperty("message", "abc(): Message (received 123)");
-		expect(abc()).toHaveProperty("prefix", "abc()");
-		expect(abc()).toHaveProperty("reason", "Message");
-		expect(abc()).toHaveProperty("value", 123);
+	describe("Value", () => {
+		test("Return correct error with reason and value (named function)", () => {
+			function abc() {
+				const e = new ValueError("Reason", 123);
+				expect(e).toHaveProperty("name", "ValueError");
+				expect(e).toHaveProperty("message", "abc(): Reason (received 123)");
+				expect(e).toHaveProperty("prefix", "abc()");
+				expect(e).toHaveProperty("reason", "Reason");
+				expect(e).toHaveProperty("value", 123);
+			}
+			abc();
+		});
+		test("Return correct error with reason and value (anonymous function)", () => {
+			(() => {
+				const e = new ValueError("Reason", 123);
+				expect(e).toHaveProperty("message", "Reason (received 123)");
+				expect(e).toHaveProperty("prefix", "");
+				expect(e).toHaveProperty("reason", "Reason");
+				expect(e).toHaveProperty("value", 123);
+			})();
+		});
 	});
-	test("Return correct error with message, value, and prefix", () => {
-		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("name", "ValueError");
-		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty(
-			"message",
-			"Object.test(): Prefix: Message (received 123)"
-		);
-		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("prefix", "Object.test(): Prefix");
-		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("reason", "Message");
-		expect(new ValueError("Message", 123, "Prefix")).toHaveProperty("value", 123);
-		function abc() {
-			return new ValueError("Message", 123, "Prefix");
-		}
-		expect(abc()).toHaveProperty("name", "ValueError");
-		expect(abc()).toHaveProperty("message", "abc(): Prefix: Message (received 123)");
-		expect(abc()).toHaveProperty("prefix", "abc(): Prefix");
-		expect(abc()).toHaveProperty("reason", "Message");
-		expect(abc()).toHaveProperty("value", 123);
+	describe("Prefix", () => {
+		test("Return correct error with reason, value, and prefix (named function)", () => {
+			function abc() {
+				const e = new ValueError("Reason", 123, "Prefix");
+				expect(e).toHaveProperty("name", "ValueError");
+				expect(e).toHaveProperty("message", "abc(): Prefix: Reason (received 123)");
+				expect(e).toHaveProperty("prefix", "abc(): Prefix");
+				expect(e).toHaveProperty("reason", "Reason");
+				expect(e).toHaveProperty("value", 123);
+			}
+			abc();
+		});
+		test("Return correct error with reason, value, and prefix (named function but with prefix including ':' colon)", () => {
+			function abc() {
+				const e = new ValueError("Reason", 123, "myFunc(): Prefix");
+				expect(e).toHaveProperty("name", "ValueError");
+				expect(e).toHaveProperty("message", "myFunc(): Prefix: Reason (received 123)");
+				expect(e).toHaveProperty("prefix", "myFunc(): Prefix");
+				expect(e).toHaveProperty("reason", "Reason");
+				expect(e).toHaveProperty("value", 123);
+			}
+			abc();
+		});
+		test("Return correct error with reason, value, and prefix (anonymous function)", () => {
+			(() => {
+				const e = new ValueError("Reason", 123, "Prefix");
+				expect(e).toHaveProperty("message", "Prefix: Reason (received 123)");
+				expect(e).toHaveProperty("prefix", "Prefix");
+				expect(e).toHaveProperty("reason", "Reason");
+				expect(e).toHaveProperty("value", 123);
+			})();
+		});
 	});
 	test("Does not show value in message if value is EMPTY", () => {
-		expect(new ValueError("Message", EMPTY).hasOwnProperty("value")).toBe(false);
-		expect(new ValueError("Message", EMPTY).message).not.toMatch(/received/);
+		expect(new ValueError("Reason", EMPTY).hasOwnProperty("value")).toBe(false);
+		expect(new ValueError("Reason", EMPTY).message).not.toMatch(/received/);
 	});
 });
